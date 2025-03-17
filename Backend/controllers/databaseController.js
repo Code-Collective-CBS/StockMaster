c4e726c1f965bb84fc9470c28b5ddc3fe20b744a
Asger Holme Håkonsson <asgerholme@github.com>
2025-03-17 19:01:59 +0100

    Begyndt på portfolio connection til DB. P.S. hav en skøn aften, vennerne. Kærlig Hilsen Asger<3

--- Backend/controllers/databaseController.js
+++ Backend/controllers/databaseController.js
@@ -6,32 +6,56 @@ const createUser = async (req, res) => {
     try {
         const pool = await poolPromise;
 
-        // Creates a query to insert users into the DB
-        const query = 
-                `INSERT INTO Users (firstname, lastname, email, password, phone_number, country_code, create_date)
-                VALUES ('${firstname}', '${lastname}', '${email}', '${password}', '${phone_number}', '${country_code}', GETDATE())`
+        // Checks if user already exists
+        const userExists = await pool.request()
 
-                // Creates and sends a query to the DB
-                await pool.request().query(query);
-                
-/*
-        // SQL-query med parameterized input (for sikkerhed)
-        await pool.request()
-            .input("firstname", sql.NVarChar(50), firstname)
-            .input("lastname", sql.NVarChar(50), lastname)
+            // "@-symbol" defines parameters in a SQL-query. More safe, optimizing query and prevents SQL injection
             .input("email", sql.NVarChar(100), email)
-            .input("password", sql.NVarChar(255), password) // Skal hashes i fremtiden
-            .input("phone_number", sql.NVarChar(20), phone_number)
-            .input("country_code", sql.NVarChar(5), country_code)
-            .query(`
-            INSERT INTO Users (firstname, lastname, email, password, phone_number, country_code, create_date)
-            VALUES (@firstname, @lastname, @email, @password, @phone_number, @country_code, GETDATE())
-        `);
-*/
+            .query("SELECT COUNT(*) AS count FROM Users WHERE email = @email");
+
+        if (userExists.recordset[0].count) {
+            return res.status(400).json({ message: "Email already exists" });
+        }
+
+                // SQL-query med parameterized input (for sikkerhed)
+                await pool.request()
+                    .input("firstname", sql.NVarChar(50), firstname)
+                    .input("lastname", sql.NVarChar(50), lastname)
+                    .input("email", sql.NVarChar(100), email)
+                    .input("password", sql.NVarChar(255), password) // Skal hashes i fremtiden
+                    .input("phone_number", sql.NVarChar(20), phone_number)
+                    .input("country_code", sql.NVarChar(5), country_code)
+                    .query(`
+                    INSERT INTO Users (firstname, lastname, email, password, phone_number, country_code, create_date)
+                    VALUES (@firstname, @lastname, @email, @password, @phone_number, @country_code, GETDATE())
+                `);
         res.status(201).json({ message: 'User created ' });
     } catch (err) {
         console.log(err);
         res.status(500).json({ message: 'Fail to create user' });
+    }
+};
+
+
+// Accounts / Portfolio API-endpoints:
+
+const getPortfolio = async (req, res) => {
+    const { user_id } = req.params; // Fetches values from URL (GET-requests)
+
+    try {
+        const pool = await poolPromise;
+
+        const result = await pool.request()
+            .input("user_id", sql.Int, user_id) // sql.Int parses ID as a number 
+            .query(`SELECT id, account_name, currency, balance, create_date, state
+                    FROM Accounts
+                    WHERE user_id = @user_id`);
+
+
+                    res.status(200).json(result.recordset);
+    } catch(err) {
+        console.log("Fail to load portfolio", err);
+        res.status(500).json({ message: "Error fetching portfolio" })
     }
 };
 
